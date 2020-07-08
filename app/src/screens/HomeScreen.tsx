bb83e6896af48b01a79b5b9aebfa50609e7a4b72
Roman Kuzmín <roman.kuzmin@ranako.cz>
2020-07-08 22:43:02 +0200

    feat: header with language select

--- app/src/screens/HomeScreen.tsx
+++ app/src/screens/HomeScreen.tsx
@@ -1,7 +1,57 @@
-import React, { FC } from 'react';
+import { Container, Drawer, Hidden, Theme } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import { createStyles } from '@material-ui/styles';
+import React, { FC, useCallback, useState } from 'react';
+import AppTitle from '../components/AppTitle';
+import Header from '../components/Header';
+import { LocaleSelect } from '../components/Locale';
+import LocaleList from '../components/Locale/LocaleList';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            marginTop: theme.spacing(2),
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        drawer: {
+            width: 250,
+        },
+    }),
+);
 
 const HomeScreen: FC = () => {
-    return <div>HomeScreen</div>;
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+
+    const handleMenu = useCallback(() => {
+        setOpen(!open);
+    }, [open]);
+
+    return (
+        <>
+            <Header>
+                <Hidden smUp>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" onClick={handleMenu}>
+                        <MenuIcon />
+                    </IconButton>
+                </Hidden>
+                <AppTitle />
+                <Hidden xsDown>
+                    <LocaleSelect />
+                </Hidden>
+            </Header>
+            <Container maxWidth="md" className={classes.container}>
+                HomeScreen
+            </Container>
+            <Drawer anchor="left" open={open} onClose={handleMenu}>
+                <LocaleList />
+            </Drawer>
+        </>
+    );
 };
 
 export default HomeScreen;
