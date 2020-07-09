23aa03e58dba82aec32ffe6e7a0d558e68e71ffa
Roman Kuzmín <roman.kuzmin@ranako.cz>
2020-07-09 16:46:17 +0200

    feat: consume production GraphQL

--- app/src/config/index.ts
+++ app/src/config/index.ts
@@ -2,9 +2,11 @@ import messages_en from '../i18n/en.json';
 import messages_cs from '../i18n/cs.json';
 import messages_de from '../i18n/de.json';
 
+const isDevelop = process.env.NODE_ENV === 'development';
+
 export default {
     api: {
-        url: 'http://localhost:4000/graphql',
+        url: isDevelop ? 'http://localhost:4000/graphql' : 'https://purple-currency-conversion.herokuapp.com/graphql',
     },
     i18n: {
         locales: {
