b966eaf55abb0bb5e01b5657b8413878a8419771
Roman Kuzmín <roman.kuzmin@ranako.cz>
2020-07-09 16:15:38 +0200

    feat: converted currency dashboard

--- app/src/screens/HomeScreen.tsx
+++ app/src/screens/HomeScreen.tsx
@@ -3,10 +3,11 @@ import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import { createStyles } from '@material-ui/styles';
-import React, { FC, useCallback, useState } from 'react';
+import React, { FC, useCallback, useEffect, useState } from 'react';
 import AppTitle from '../components/AppTitle';
 import { ConvertCurrency } from '../components/ConvertCurrency';
 import { ConvertedAmount } from '../components/ConvertedAmount';
+import { ConvertedCurrencyDashboard } from '../components/ConvertedCurrency';
 import Header from '../components/Header';
 import { LocaleSelect } from '../components/Locale';
 import LocaleList from '../components/Locale/LocaleList';
@@ -33,18 +34,29 @@ type ConvertedAmountState = {
 const HomeScreen: FC = () => {
     const classes = useStyles();
     const [open, setOpen] = useState(false);
+    const [refreshDashboard, setRefreshDashboard] = useState(false);
     const [convertedAmount, setConvertedAmount] = useState<ConvertedAmountState>();
+
+    useEffect(() => {
+        if (refreshDashboard) {
+            setRefreshDashboard(false);
+        }
+    }, [refreshDashboard]);
 
     const handleMenu = useCallback(() => {
         setOpen(!open);
     }, [open]);
 
-    const handleConverted = useCallback((amount, currency) => {
-        setConvertedAmount({
-            amount,
-            currency,
-        });
-    }, []);
+    const handleConverted = useCallback(
+        (amount, currency) => {
+            setConvertedAmount({
+                amount,
+                currency,
+            });
+            setRefreshDashboard(true);
+        },
+        [setRefreshDashboard],
+    );
 
     return (
         <>
@@ -62,6 +74,7 @@ const HomeScreen: FC = () => {
             <Container maxWidth="md" className={classes.container}>
                 <ConvertCurrency onConverted={handleConverted} />
                 {convertedAmount && convertedAmount.amount ? <ConvertedAmount {...convertedAmount} /> : null}
+                <ConvertedCurrencyDashboard refresh={refreshDashboard} />
             </Container>
             <Drawer anchor="left" open={open} onClose={handleMenu}>
                 <LocaleList />
