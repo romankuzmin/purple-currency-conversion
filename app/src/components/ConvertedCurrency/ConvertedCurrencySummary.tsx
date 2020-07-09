b4ba2abd1754f1800242a2d64b1c4b3a0a76cec9
Roman Kuzmín <roman.kuzmin@ranako.cz>
2020-07-09 18:21:37 +0200

    refactor: remove unused

--- app/src/components/ConvertedCurrency/ConvertedCurrencySummary.tsx
+++ app/src/components/ConvertedCurrency/ConvertedCurrencySummary.tsx
@@ -1,9 +1,9 @@
-import { Card, CardContent, Grid, Theme, Typography } from '@material-ui/core';
+import { Card, CardContent, Theme, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Skeleton } from '@material-ui/lab';
 import { createStyles } from '@material-ui/styles';
 import React, { FC, memo } from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
