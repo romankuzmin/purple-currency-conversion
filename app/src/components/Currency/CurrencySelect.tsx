8c73197a3b5e048c31a457f86b0865dfeab78522
Roman Kuzmín <roman.kuzmin@ranako.cz>
2020-07-22 21:18:10 +0200

    feat: remove error state and refreshing

--- app/src/components/Currency/CurrencySelect.tsx
+++ app/src/components/Currency/CurrencySelect.tsx
@@ -1,21 +1,11 @@
-import { useQuery } from '@apollo/react-hooks';
-import {
-    CircularProgress,
-    FormControl,
-    FormHelperText,
-    IconButton,
-    InputLabel,
-    MenuItem,
-    Theme,
-} from '@material-ui/core';
+import { FormControl, InputLabel, MenuItem, Theme } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Refresh as RefreshIcon } from '@material-ui/icons';
 import { createStyles } from '@material-ui/styles';
-import { gql } from 'apollo-boost';
 import { Field } from 'formik';
 import { Select } from 'formik-material-ui';
-import React, { FC, memo, useCallback } from 'react';
+import React, { FC, memo } from 'react';
 import { FormattedMessage } from 'react-intl';
+import useCurrenciesQuery from '../../hooks/useCurrenciesQuery';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -23,79 +13,23 @@ const useStyles = makeStyles((theme: Theme) =>
             width: '100%',
             maxWidth: theme.spacing(18),
         },
-        fieldProgress: {
-            '&> div': {
-                paddingBottom: theme.spacing(1.7),
-            },
-            '&> svg': {
-                display: 'none',
-            },
-        },
-        progress: {
-            marginLeft: theme.spacing(2),
-        },
-        fieldError: {
-            '&> div': {
-                paddingRight: `${theme.spacing(2)}px !important`,
-                paddingBottom: theme.spacing(1.7),
-                display: 'flex',
-                justifyContent: 'center',
-            },
-            '&> svg': {
-                display: 'none',
-            },
-        },
-        iconButton: {
-            padding: 0,
-        },
     }),
 );
-
-const CURRENCIES = gql`
-    {
-        currencies {
-            code
-            title
-        }
-    }
-`;
-
-type Currency = {
-    code: string;
-    title: string;
-};
 
 type CurrencySelectProps = {
     name: string;
     label: string;
+    disabled?: boolean;
 };
 
-const CurrencySelect: FC<CurrencySelectProps> = ({ name, label }) => {
-    const { loading, error, data, refetch } = useQuery(CURRENCIES);
+const CurrencySelect: FC<CurrencySelectProps> = ({ name, label, disabled }) => {
+    const { currencies } = useCurrenciesQuery();
     const classes = useStyles();
-    const currencies = data ? (data.currencies as Currency[]) : [];
     const id = `${name}-select`;
     const labelId = `${id}-label`;
 
-    const handleRefresh = useCallback(async () => {
-        refetch();
-    }, [refetch]);
-
-    let valueToRender = loading && <CircularProgress color="primary" size={20} className={classes.progress} />;
-
-    valueToRender =
-        !valueToRender && error ? (
-            <IconButton color="secondary" size="small" className={classes.iconButton} onClick={handleRefresh}>
-                <RefreshIcon />
-            </IconButton>
-        ) : (
-            valueToRender
-        );
-
-    const fieldClass = loading ? classes.fieldProgress : '';
-
     return (
-        <FormControl variant="outlined" className={classes.root} error={!!error}>
+        <FormControl variant="outlined" className={classes.root} disabled={disabled}>
             <InputLabel htmlFor={labelId}>
                 <FormattedMessage id={label} />
             </InputLabel>
@@ -103,12 +37,11 @@ const CurrencySelect: FC<CurrencySelectProps> = ({ name, label }) => {
                 labelId={labelId}
                 component={Select}
                 name={name}
-                className={error ? classes.fieldError : fieldClass}
                 inputProps={{
                     id,
                 }}
                 label={<FormattedMessage id={label} />}
-                renderValue={(value: string) => (valueToRender ? valueToRender : value)}
+                disabled={disabled}
             >
                 {currencies.map((currency) => (
                     <MenuItem key={currency.code} value={currency.code}>
@@ -116,11 +49,6 @@ const CurrencySelect: FC<CurrencySelectProps> = ({ name, label }) => {
                     </MenuItem>
                 ))}
             </Field>
-            {error ? (
-                <FormHelperText>
-                    <FormattedMessage id="app.currencySelect.errors.fetchError" />
-                </FormHelperText>
-            ) : null}
         </FormControl>
     );
 };
